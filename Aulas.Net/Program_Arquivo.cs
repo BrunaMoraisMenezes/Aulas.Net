1216111e98f8d11a1a63f061a925ac6164fa73fa
BrunaMoraisMenezes <58346646+BrunaMoraisMenezes@users.noreply.github.com>
2022-06-24 08:55:35 -0300

    Add files via upload

---
+++ Aulas.Net/Program_Arquivo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Arquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o endereco: ");
+            string endereco = Console.ReadLine();
+
+            Arquivo arquivo = new Arquivo(endereco);
+
+            try
+            {
+                arquivo.ListaDiretorios();
+                arquivo.ListaFuncionarios();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Endereco Inválido");
+            }
+        }
+    }
+}
